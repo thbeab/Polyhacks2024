c3995c59f933ed8988f75be62cfcfa6d4e5855ef
Thomas <thomas-frederic.beaugendre@polymtl.ca>
2024-02-02 23:05:48 -0500

    test chatroom firestore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,39 @@
 import { Component, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Firestore, collection, collectionData, doc, addDoc} from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  firestore: Firestore = inject(Firestore);
   title = 'polyhacks';
 
-  items$: Observable<any[]>;
+  messages$: Observable<any[]> = new Observable<any[]>();
+  newMessage = {content: ''};
+  messagesRef: any;
+  
 
-  constructor() {
-    const aCollection = collection(this.firestore, 'items')
-    this.items$ = collectionData(aCollection);
+  roomId: string = '';
+
+  constructor(private firestore: Firestore) {
+
   }
+
+  joinRoom() {
+    this.messagesRef = collection(this.firestore, `rooms/${this.roomId}/messages`)
+    this.messages$ = collectionData(this.messagesRef, {idField: 'id'});
+  }
+
+  sendMessage() {
+    addDoc(this.messagesRef, this.newMessage);
+    this.newMessage.content = '';
+  }
+
 }
